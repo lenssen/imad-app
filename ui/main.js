3989f9dcf23b0cf44e505ba92eb060e7761f3105
lenssen <manasae.lenssen@gmail.com>
2017-08-16 21:22:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML="changed text"
+var img = document.getElementById('madi');
+img.onclick = function() {
+  img.style.marginleft='100px';  
+};
