5395b73b48c813fd6adc3f8bec2137cf92a56293
lenssen <manasae.lenssen@gmail.com>
2017-08-19 21:23:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,12 @@ function createtemplate (data) {
 `;
     return htmltemplate;
 }
+var counter=0;
+app.get('/counter', function(req,res)
+{
+   counter=counter+1;
+   res.send(counter,tostring());
+});
 app.get('/:articlename', function(req,res){
    var articlename= req.params.articlename;
     res.send(createtemplate(articles[articlename]));
