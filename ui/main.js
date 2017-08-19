72d32755fc669e6a7dd54f4502556459709a8302
lenssen <manasae.lenssen@gmail.com>
2017-08-19 21:46:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,14 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML="changed text"
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-        marginLeft= marginLeft + 1;
-        img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-  var interval = setInterval(moveRight,100);
+var counter= 0;
+var button = document.getElementById('counter');
+button.onclick= function(){
+    
+    
+    
+    
+    
+    
+    counter=counter+1;
+    var span =document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 };
-v
