37bd7a0b99abb2cf2768352d53907c1feaaa6b16
lenssen <manasae.lenssen@gmail.com>
2017-08-19 22:14:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ button.onclick= function(){
         }
     }
     };
-    request.open('GET','http://manasaelenssen.imad.hasrua-app.io/counter',true);
+    request.open('GET','http://manasaelenssen.imad.hasura-app.io/counter',true);
             request.send(null);
     
 
