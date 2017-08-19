ab8b56de7ecb08e4e3e6116cf7412b5d056ffb71
lenssen <manasae.lenssen@gmail.com>
2017-08-19 21:31:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ var counter = 0;
 app.get('/counter', function(req,res)
 {
    counter = counter+1;
-   res.send(counter,toString());
+   res.send(counter.toString());
 });
 app.get('/:articlename', function(req,res){
    var articlename= req.params.articlename;
