ed12d5777300096f0f2585d0c752f70763bed167
lenssen <manasae.lenssen@gmail.com>
2017-08-20 11:49:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,18 @@ button.onclick= function(){
 
     
 };
+ 
+ 
+ var nameInput= document.getElementById('name');
+ var name= nameInput.value;
+ var submit=document.getElementById('submitbtn');
+ submit.onclick=function(){
+   var names=['name1','name2','name3','name4'];
+   var list='';
+   for(var i=0;i<names.length;i++)
+   {
+       list += '<li>'+names[li]+'</li>';
+   }
+   var ul= document.getElementById('namelist');
+   ul.innerHTML=list;
+ };
