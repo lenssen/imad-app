c383183e79c341e4f90abc86c0cd0bb7936beee3
lenssen <manasae.lenssen@gmail.com>
2017-08-20 12:03:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+var names=[];
+app.get('/submitname',function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 var articles ={
 'articleone':{
         title :'article-one',
@@ -100,12 +107,6 @@ app.get('/:articlename', function(req,res){
 });
 app.get('/ui/main.js', function (req,res){
   res.sendFile(path.join(__dirname,'ui','main.js'));  
-});
-var names=[];
-app.get('/submitname',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
 });
 
 
